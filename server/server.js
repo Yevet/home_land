591280c890b7a9d6cfb29c734be012bf8d60cd6e
Yevet <xiangyiwen0218@gmail.com>
2023-09-21 01:50:33 -0700

    email-sent

--- server/server.js
+++ server/server.js
@@ -1,16 +1,23 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import { DATABASE } from './config.js';
+
+import authRoutes from './routes/auth.js';
 
 const app = express();
+
+
+mongoose.connect(DATABASE)
+        .then(()=>console.log('DB connected'))
+        .catch(err=>console.log(err));
+
+app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
+app.use('/api',authRoutes);
 
-app.get('/api',(req,res)=>{
-    res.json({
-        data:'Hello World',
 
-    });
-});
 
 app.listen(8000,()=>console.log('Server is running on port 8000'));
