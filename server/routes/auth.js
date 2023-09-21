cecc5c161adef44f2cee367ec196a5f3ba7fb4d5
Yevet <xiangyiwen0218@gmail.com>
2023-09-21 01:56:58 -0700

    dynamic email

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 
 router.get('/', auth.welcome);
-router.post('/pre-register',auth.preRegister);
+router.post('/pre-register', auth.preRegister);
+router.post('/register', auth.register);
 
 
 export default router;
