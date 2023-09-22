c01e6cd592a18d1af04cff10c4a4aed820a0909a
Yevet <xiangyiwen0218@gmail.com>
2023-09-22 03:35:37 -0700

    hashsalt password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,9 @@
 import * as config from "../config.js";
 import jwt from "jsonwebtoken";
 import { emailTemplate } from "../helpers/email.js";
+import { hashPassword, comparePassword } from "../helpers/auth.js";
+import User from "../models/user.js";
+import { nanoid } from "nanoid";
 
 export const welcome = (req, res) => {
   res.json({
@@ -48,6 +51,30 @@ export const register = async (req, res) => {
   try {
     // console.log(req.body);
     const { email, password } = jwt.verify(req.body.token, config.JWT_SECRET);
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = await new User({
+      username: nanoid(6),
+      email,
+      password: hashedPassword,
+    }).save();
+
+    const token = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    const refreshToken = jwt.sign({ _id: user._id }, config.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    user.password = undefined;
+    user.resetCode = undefined;
+
+    return res.json({
+      token,
+      refreshToken,
+      user,
+    });
   } catch (err) {
     console.log(err);
     return res.json({ error: "Something went wrong. Try again." });
