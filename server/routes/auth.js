d84924bf0f091b17fee7138411a6912528e61dac
Yevet <xiangyiwen0218@gmail.com>
2023-09-22 04:11:49 -0700

    protect routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,18 @@
-
 import express from "express";
-import * as auth from "../controllers/auth.js";
+
 const router = express.Router();
 
+import * as auth from "../controllers/auth.js";
+import { requireSignin } from "../middlewares/auth.js";
 
-router.get('/', auth.welcome);
-router.post('/pre-register', auth.preRegister);
-router.post('/register', auth.register);
-router.post('/login', auth.login);
-router.post('/forgot-password', auth.forgotPassword);
-router.post('/access-account', auth.accessAccount);
+router.get("/", requireSignin, auth.welcome);
+router.post("/pre-register", auth.preRegister);
+router.post("/register", auth.register);
+router.post("/login", auth.login);
+router.post("/forgot-password", auth.forgotPassword);
+router.post("/access-account", auth.accessAccount);
+router.get("/refresh-token", auth.refreshToken);
+router.get("/current-user", requireSignin, auth.currentUser);
+router.get("/profile/:username", auth.publicProfile);
+
 export default router;
