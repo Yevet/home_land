86eb2e609d7bbdb5f6884e4701bcf930107e392c
Yevet <xiangyiwen0218@gmail.com>
2023-09-22 22:37:26 -0700

    toast

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/auth";
+import Main from "./components/nav/Main";
+import { Toaster } from "react-hot-toast";
 
-
-
-import Main from "./components/nav/Main"
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -11,6 +10,8 @@ import Register from "./pages/Register";
 function App() {
   return (
     <BrowserRouter>
+      <Main />
+      <Toaster />
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
