fbfe87fac15d0d17f3955671923960d2694ac2c7
Yevet <xiangyiwen0218@gmail.com>
2023-09-23 18:43:17 -0700

    forget password

--- client/src/components/nav/Main.js
+++ client/src/components/nav/Main.js
@@ -14,37 +14,51 @@ export default function Main() {
         navigate("/login");
     };
 
+    const loggedIn =
+        auth.user !== null && auth.token !== "" && auth.refreshToken !== "";
+
     return (
         <nav className="nav d-flex justify-content-between lead">
             <NavLink className="nav-link" aria-current="page" to="/">
                 Home
             </NavLink>
-            <NavLink className="nav-link" to="/login">
-                Login
-            </NavLink>
-            <NavLink className="nav-link" to="/register">
-                Register
-            </NavLink>
 
-            <div className="dropdown">
-                <li>
-                    <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
-                        User
-                    </a>
-                    <ul className="dropdown-menu">
-                        <li>
-                            <NavLink className="nav-link" to="/dashboard">
-                                Dashboard
-                            </NavLink>
-                        </li>
-                        <li>
-                            <a onClick={logout} className="nav-link">
-                                Logout
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-            </div>
+            {!loggedIn ? (
+                <>
+                    <NavLink className="nav-link" to="/login">
+                        Login
+                    </NavLink>
+                    <NavLink className="nav-link" to="/register">
+                        Register
+                    </NavLink>
+                </>
+            ) : (
+                ""
+            )}
+
+            {loggedIn ? (
+                <div className="dropdown">
+                    <li>
+                        <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown">
+                            {auth?.user?.name ? auth.user.name : auth.user.username}
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li>
+                                <NavLink className="nav-link" to="/dashboard">
+                                    Dashboard
+                                </NavLink>
+                            </li>
+                            <li>
+                                <a onClick={logout} className="nav-link">
+                                    Logout
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                </div>
+            ) : (
+                ""
+            )}
         </nav>
     );
 }
