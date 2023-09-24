e133229e3c3b4af4c63b5d347c8bec023d0b252e
Yevet <xiangyiwen0218@gmail.com>
2023-09-23 19:00:49 -0700

    privacy route

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import AccountActivate from "./pages/auth/AccountActivate";
 import ForgotPassword from "./pages/auth/ForgotPassword";
 import AccessAccount from "./pages/auth/AccessAccount";
 import Dashboard from "./pages/user/Dashboard";
+import AdCreate from "./pages/user/ad/AdCreate";
+import PrivateRoute from "./components/routes/PrivateRoute";
 
 function App() {
   return (
@@ -30,7 +32,11 @@ function App() {
             path="/auth/access-account/:token"
             element={<AccessAccount />}
           />
-          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="ad/create" element={<AdCreate />} />
+          </Route>
         </Routes>
       </AuthProvider>
     </BrowserRouter>
